584f32a8081b42ad1a629d4a867c26c9c24311b1
wenhanshi <191063623@qq.com>
2017-04-13 17:04:49 +0800

    fuck yang

--- Server/simulate.py
+++ Server/simulate.py
@@ -6,15 +6,35 @@ MINOR_TIME_SLOT = 0.02
 open_flags = [True, True, True, True]
 runnings = [0, 1, 3]
 waitings = [2]
+insts_queue = []
+hang_queue = []
+insts = []
+rooms = []
+
+
+def dispatch(runnings, status):
+    pass
+    return runnings, status
+
+
+def fetch_insts(runnings, waitings, insts_queue):
+    pass
+    return insts, hangs
+
+
+def exec_inst(inst, status):
+    pass
+    return
+
 
 def simulate():
-
     while True:
-        if count_open_client(open_flags) > 3:
-            runnings = dispatch(runnings, status)
-            waitings = get_waiting(runnings, clients)
-        insts = fetch_insts(runnings, insts_queue)
-        update_hang_queue(hang_queue, waiting, insts_queue)
+        if sum(open_flags) > 3:
+            runnings, waitings = dispatch(runnings, rooms)
+        insts, hangs = fetch_insts(runnings, waitings, insts_queue)
+        hang_queue.append(hangs)
         for inst in insts:
-            exec_inst(inst, status)
+            exec_inst(inst, rooms)
+        for room in rooms:
+            room.sim()
         time.sleep(MINOR_TIME_SLOT)
