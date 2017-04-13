9418018b68539abd505039ca7a7532075bea3f58
wenhanshi <191063623@qq.com>
2017-04-13 16:39:30 +0800

    fuck yang

--- Server/simulate.py
+++ Server/simulate.py
@@ -1,7 +1,20 @@
 import threading
+import time
 
 MINOR_TIME_SLOT = 0.02
 
+open_flags = [True, True, True, True]
+runnings = [0, 1, 3]
+waitings = [2]
+
 def simulate():
+
     while True:
-        pass
+        if count_open_client(open_flags) > 3:
+            runnings = dispatch(runnings, status)
+            waitings = get_waiting(runnings, clients)
+        insts = fetch_insts(runnings, insts_queue)
+        update_hang_queue(hang_queue, waiting, insts_queue)
+        for inst in insts:
+            exec_inst(inst, status)
+        time.sleep(MINOR_TIME_SLOT)
