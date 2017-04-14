69ec901a88d3b7059970e9723b9f3fc9c0e81a03
676259299@qq.com <676259299@qq.com>
2017-04-14 19:53:37 +0800

    Add Server front-end files.

---
+++ Server/View/app-services/commonsetting.service.js
@@ -0,0 +1,58 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('CommonSettingService', CommonSettingService);
+
+    function CommonSettingService($http, $rootScope) {
+        var service = {};
+        service.getInitCommonSetting = getInitCommonSetting;
+        service.setPricePerMin = setPricePerMin;
+        service.setTemperature = setTemperature;
+        service.setSpeed = setSpeed;
+        return service;
+    
+        function getInitCommonSetting() {
+            return $http.post($rootScope.getUrl + 'get_init', {
+            }).then(handleSuccess, handleError('Error getInitCommonService'));
+            //　返回ｊｓｏｎ如下：
+            // ｛　mode　: mode, lowTemp : lowTemp, highTemp : highTemp, defaultTemp : defaultTemp,
+            //    lowSpeed : lowSpeed, midSpeed : midSpeed, highSpeed : highSpeed. pricePerMin : pricePerMin };
+        }
+
+        function setPricePerMin(price) {
+            return $http.post($rootScope.getUrl + 'set_price', {
+                pricePerMin : price
+            }).then(handleSuccess, handleError('Error tetPricePerMin'));
+        }
+
+        function setTemperature(mode, lowTemp, highTemp, defaultTemp) {
+            return $http.post($rootScope.getUrl + 'set_temperature', {
+                mode : mode,
+                lowTemp : lowTemp,
+                highTemp : highTemp,
+                defaultTemp : defaultTemp
+            }).then(handleSuccess, handleError('Error setTemperature'));
+        }
+
+        function setSpeed(lowSpeed, midSpeed, highSpeed) {
+            return $http.post($rootScope.getUrl + 'set_speed', {
+                lowSpeed : lowSpeed,
+                midSpeed : midSpeed,
+                highSpeed : highSpeed
+            }).then(handleSuccess, handleError('Error setSpeed'));
+        }
+ 
+        // private functions
+        function handleSuccess(res) {
+            return { success: true, data: res };
+        }
+
+        function handleError(error) {
+            return function() {
+                return { success: false, message: error };
+            };
+        }
+    }
+})();
